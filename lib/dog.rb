44fbb58e997434a9ff439a7c4161f4b1ce83bceb
Cody Frank <cody.frank30@gmail.com>
2019-05-08 00:27:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(name)
+    @dogs_name = name
+  end
+  def name
+    @dogs_name
+  end
+  def breed= (breed)
+    @dog_breed = breed
+  end
+
+  def breed
+    @dog_breed
+  end
+end
